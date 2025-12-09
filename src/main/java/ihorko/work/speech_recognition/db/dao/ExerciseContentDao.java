9efd487f4ef6d5f94bf2e4e82849ede03acedc67
Ascomos21 <kucherjavyj228@gmail.com>
2025-12-09 20:00:33 +0200

    fixes dao

--- src/main/java/ihorko/work/speech_recognition/db/dao/ExerciseContentDao.java
+++ src/main/java/ihorko/work/speech_recognition/db/dao/ExerciseContentDao.java
@@ -1,10 +1,12 @@
 package ihorko.work.speech_recognition.db.dao;
 
 import ihorko.work.speech_recognition.db.entity.ExerciseContent;
-import org.hibernate.SessionFactory;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.List;
@@ -14,36 +16,42 @@ import java.util.UUID;
 @Transactional
 public class ExerciseContentDao {
 
-    private final SessionFactory sessionFactory;
+    private final EntityManager entityManager;
 
     @Autowired
-    public ExerciseContentDao(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+    public ExerciseContentDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
-    public void persist(ExerciseContent ExerciseContent) {
-        sessionFactory.getCurrentSession().saveOrUpdate(ExerciseContent);
+    private Session getSession() {
+        return entityManager.unwrap(Session.class);
+    }
+
+    public void persist(ExerciseContent exerciseContent) {
+        getSession().saveOrUpdate(exerciseContent);
     }
 
     public List<ExerciseContent> listExercisesContent() {
-        TypedQuery<ExerciseContent> query = sessionFactory.getCurrentSession()
+        TypedQuery<ExerciseContent> query = getSession()
                 .createQuery("From ExerciseContent", ExerciseContent.class);
         return query.getResultList();
     }
 
     public List<ExerciseContent> listExercisesContentByExercise(UUID exercise) {
-        TypedQuery<ExerciseContent> query = sessionFactory.getCurrentSession()
-                .createQuery("from ExerciseContent s where s.exercise.id= :exercise_id", ExerciseContent.class);
+        TypedQuery<ExerciseContent> query = getSession()
+                .createQuery("from ExerciseContent s where s.exercise.id = :exercise_id", ExerciseContent.class);
         query.setParameter("exercise_id", exercise);
         return query.getResultList();
     }
 
     public ExerciseContent findById(UUID uuid) {
-        return sessionFactory.getCurrentSession()
-                .get(ExerciseContent.class, uuid);
+        return getSession().get(ExerciseContent.class, uuid);
     }
 
     public void delete(UUID uuid) {
-        sessionFactory.getCurrentSession().delete(findById(uuid));
+        ExerciseContent entity = findById(uuid);
+        if (entity != null) {
+            getSession().delete(entity);
+        }
     }
 }
