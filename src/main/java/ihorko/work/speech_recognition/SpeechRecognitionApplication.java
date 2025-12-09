c80cf5f34bfc752f78592b1636137d74de6b9bbe
Ascomos21 <kucherjavyj228@gmail.com>
2025-12-09 22:12:38 +0200

    changes in pom

--- src/main/java/ihorko/work/speech_recognition/SpeechRecognitionApplication.java
+++ src/main/java/ihorko/work/speech_recognition/SpeechRecognitionApplication.java
@@ -2,10 +2,12 @@ package ihorko.work.speech_recognition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EntityScan("ihorko.work.speech_recognition.db.entity")
 public class SpeechRecognitionApplication {
 
 	public static void main(String[] args) {
